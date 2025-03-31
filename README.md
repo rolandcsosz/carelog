87a45ab15449dc6b6416d4f143d2ce51cdf00397
Roland Csősz <hello@rolandcsosz.com>
2025-03-31 17:52:53 +0200

    Add init project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Activity Log App
+
+## Running the app in dev mode
+1. Clone the repository
+2. Run `npm install`
+3. Run `npm run dev`
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+const App : React.FC = () => {
+
+  return (
+    <div>
+      <h1>Hello Activity Log App</h1>
+    </div>
+  )
+}
+
+export default App
